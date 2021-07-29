ba79b0a9c5e5e3b5e7f972f11e8ab7d4886c9ef7
AlissonMoraesDev <alissonmoraesdev@gmail.com>
2021-07-28 21:15:27 -0300

    Desenvolvimento da mypage

---
+++ assets/js/main.js
@@ -0,0 +1,56 @@
+// Exibir menu
+const showMenu = (toggleId, navId) => {
+  const toggle = document.getElementById(toggleId),
+  nav = document.getElementById(navId);
+
+  if (toggle && navId) {
+    toggle.addEventListener('click', () => {
+      nav.classList.toggle('show')
+    })
+  }
+}
+
+showMenu('nav-toggle', 'nav-menu')
+
+// Ativar e remover o menu
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction() {
+  // Link ativo
+  navLink.forEach(n => n.classList.remove('active'))
+  this.classList.add('active')
+
+  // Remove o menu mobile
+  const navMenu = document.getElementById('nav-menu')
+  navMenu.classList.remove('show')
+
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+// Scroll reveal animation
+
+const sr = ScrollReveal({
+  origin: 'top',
+  distance: '80px',
+  duration: 2000,
+  reset: true
+})
+
+sr.reveal('.home__title', {})
+sr.reveal('.button', {delay: 200})
+sr.reveal('.home__img', {delay: 400})
+sr.reveal('.home__social-icon', {interval: 200})
+
+sr.reveal('.section-title', {delay: 100})
+
+// Scroll About 
+
+sr.reveal('.about__img', {})
+sr.reveal('.about__subtitle', {delay: 200})
+sr.reveal('.about__text', {delay: 400})
+
+// Scroll Contact
+
+sr.reveal('.contact', {delay: 200})
+sr.reveal('.contact__form', {delay: 400})
